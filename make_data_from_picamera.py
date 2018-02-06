c47bf9f9e89ae28effd2bab37ca62626ebbfa1b9
hiyayakko0 <hiyayakko0@users.noreply.github.com>
2018-02-06 23:35:46 +0900

    Update make_data_from_picamera.py

--- make_data_from_picamera.py
+++ make_data_from_picamera.py
@@ -6,6 +6,7 @@
 import picamera
 import cv2, os, argparse, shutil
 import datetime
+import time
 # parameter ###################################
 # 切り抜いた画像の保存先ディレクトリ
 save_path = r"C:\CMD\OpenCV\TEST"
@@ -27,32 +28,42 @@ faceCascade = cv2.CascadeClassifier(cascade_path)
 face_detect_count = 0
 
 # 顔検知に失敗した数(デフォルトで0を指定)
-pic_saved_count = 0
+pic_count = 0
 
 # フォルダ内ファイルを変数に格納(ディレクトリも格納)
-img = cv2.imread(???)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-face = faceCascade.detectMultiScale(gray,
-                                    scaleFactor=1.3,
-                                    minNeighbors=2, 
-                                    minSize=(50,50)
-                                    )
-if len(face) > 0:
-    for (x,y,w,h) in face:
-        # 切り取った画像出力
-        rt = 0.1
-        size = max(w,h)
-        x = max(x-int(w*rt),0)
-        y = max(y-int(h*rt),0)
-        
-        # 顔写真の保存
-        now = datetime.datetime.now()
-        ff = os.path.join(f_folder,'face_{0:%Y%m%d%H%M%S}.jpg'.format(now))
-        cv2.imwrite(ff, 
-                    img[y:y+int(size*(rt+1)),#上から下へ 
-                        x:x+int(size*(rt+1))]#右から左へ
-                    )
-        face_detect_count = face_detect_count + 1
+while True:
+    now = datetime.datetime.now()
+    pic_count += 1
+    with picamera.PiCamera() as camera:
+            # Photo dimensions and rotation
+            time.sleep(3)  # カメラ初期化
+            if pic_count % 5 == 0:
+                pf = os.path.join(p_folder,'pic_{0:%Y%m%d%H%M%S}.jpg'.format(now))
+                camera.capture(pf)
+    img = cv2.imread(???)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = faceCascade.detectMultiScale(gray,
+                                        scaleFactor=1.3,
+                                        minNeighbors=2, 
+                                        minSize=(50,50)
+                                        )
+    if len(face) > 0:
+        for (x,y,w,h) in face:
+            # 切り取った画像出力
+            rt = 0.1
+            size = max(w,h)
+            x = max(x-int(w*rt),0)
+            y = max(y-int(h*rt),0)
+
+            # 顔写真の保存
+            ff = os.path.join(f_folder,'face_{0:%Y%m%d%H%M%S}.jpg'.format(now))
+            cv2.imwrite(ff, 
+                        img[y:y+int(size*(rt+1)),#上から下へ 
+                            x:x+int(size*(rt+1))]#右から左へ
+                        )
+            face_detect_count = face_detect_count + 1
+    if face_detect_count >= 5:
+        break
 
 
 
