a35d3b615e446a2910f7f2d66248dfb160489cab
hiyayakko0 <hiyayakko0@users.noreply.github.com>
2018-02-13 13:44:50 +0900

    Update vgg16_fine_tuning.py

--- vgg16_fine_tuning.py
+++ vgg16_fine_tuning.py
@@ -1,97 +1,215 @@
 # Modules #########################################################
+from __future__ import print_function
 
 import os
-from keras.applications.vgg16 import VGG16
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, Model
-from keras.layers import Input, Activation, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
 import numpy as np
+from PIL import Image
+from argparse import ArgumentParser
 
-# Parameters #######################################################
-x_train,x_test,y_train_,y_test = np.load(r"C:\CMD\Tensorflow\Fine_Tuning\train\data.npy")
-f_size = 200
-train_data_dir = r'C:\CMD\Tensorflow\Fine_Tuning\train'
-validation_data_dir = r'C:\CMD\Tensorflow\Fine_Tuning\validation'
-samples_per_epoch = 10
-nb_val_samples = 100
-nb_epoch = 50
-result_dir = r'C:\CMD\Tensorflow\Fine_Tuning\results'
+from keras.applications.xception import Xception
+from keras.models import Model, Sequential
+from keras.layers import Dense, GlobalAveragePooling2D,Input, Activation, Dropout, Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator,load_img, img_to_array
+from keras.utils import np_utils
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras import optimizers
+from sklearn import model_selection
 
-if __name__ == '__main__':
-    # VGG16モデルと学習済み重みをロード
-    # Fully-connected層（FC）はいらないのでinclude_top=False）
-    # input_tensorを指定しておかないとoutput_shapeがNoneになってエラーになるので注意
-    # https://keras.io/applications/#inceptionv3
-    input_tensor = Input(shape=(f_size,f_size, 3))
-    vgg16_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
-    # vgg16_model.summary()
+# Parameter ##############################################################
+# 作成するデータセットパス
+# データセットパス以下に入れたフォルダがそのままカテゴリになる。
+# 00Hanako
+# 01Taro
+#　のようにつける
+dataset_path = r"C:\CMD\Tensorflow\Fine_Tuning"
 
-    # FC層を構築
-    # Flattenへの入力指定はバッチ数を除く
-    top_model = Sequential()
-    top_model.add(Flatten(input_shape=vgg16_model.output_shape[1:]))
-    top_model.add(Dense(256, activation='relu'))
-    top_model.add(Dropout(0.5))
-    top_model.add(Dense(1, activation='sigmoid'))
+# 格納する画像サイズの指定: MNISTデータセットの場合、28x28
+# Functions ############################################################## 
+  
 
-    # vgg16_modelはkeras.engine.training.Model
-    # top_modelはSequentialとなっている
-    # ModelはSequentialでないためadd()がない
-    # そのためFunctional APIで二つのモデルを結合する
-    # https://github.com/fchollet/keras/issues/4040
-    model = Model(input=vgg16_model.input, output=top_model(vgg16_model.output))
-    print('vgg16_model:', vgg16_model)
-    print('top_model:', top_model)
-    print('model:', model)
 
-    # layerを表示
-    for i in range(len(model.layers)):
-        print(i, model.layers[i])
 
-    # 最後のconv層の直前までの層をfreeze
-    for layer in model.layers[:15]:
+def Parser():
+    parser = ArgumentParser()
+    parser.add_argument('--image_size', default=299)
+    parser.add_argument('--validation_set', default=1)
+    parser.add_argument('--dataset_path', default=r"C:\CMD\Tensorflow\Fine_Tuning")
+    parser.add_argument('--batch_size_pre', default=32)
+    parser.add_argument('--batch_size_fine', default=16)
+    parser.add_argument('--nb_epoch_pre', default=5)
+    parser.add_argument('--nb_epoch_fine', default=100)
+    parser.add_argument('--output_path', default='output')
+    parser.add_argument('--device_id', default=0)
+
+    return parser
+
+class DataLoader():
+    def __init__(self, image_size,dataset_path):
+        self.image_size = image_size
+        self.dataset_path = dataset_path
+
+    def load_data(self):
+        categories = [
+                   fls 
+                   for fls in os.listdir(os.path.join(self.dataset_path,"data"))
+                   if os.path.isdir(os.path.join(self.dataset_path,"data",fls))
+                    ]
+            
+        nb_classes = len(categories)
+        
+        datas =[
+                [os.path.join(self.dataset_path+"\\data",name,j),i]
+                for i,name in enumerate(categories)
+                for j in os.listdir(os.path.join(self.dataset_path+"\\data",name))
+                ]
+        X = []
+        Y = []
+        for data in datas:
+                img = load_img(data[0], target_size=(image_size,image_size))
+                img = img_to_array(img)
+                X.append(img)
+                Y.append(data[1])
+        X = np.array(X)
+        Y = np.array(Y)
+        
+        X_train, X_test, y_train, y_test = \
+            model_selection.train_test_split(X, Y)
+        xy = (X_train, X_test, y_train, y_test)
+        np.save(os.path.join(dataset_path,"data.npy"), xy)
+        print("ok,", len(Y))
+
+        return (X_train,y_train), (X_test,y_test), nb_classes
+
+def createImageDataGenerator():
+    return ImageDataGenerator(
+        rescale=1./255,
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=5,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.125,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.125,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        zoom_range=0.5,
+        vertical_flip=False)  # randomly flip images
+
+if __name__ == "__main__":
+
+    # contants
+    
+    # parse args
+    options = Parser().parse_args()
+
+    image_size = int(options.image_size)
+    validation_set = int(options.validation_set)
+    dataset_path = options.dataset_path
+    batch_size_pre = int(options.batch_size_pre)
+    batch_size_fine = int(options.batch_size_fine)
+    nb_epoch_pre = int(options.nb_epoch_pre)
+    nb_epoch_fine = int(options.nb_epoch_fine)
+    output_path = os.path.join(options.dataset_path,options.output_path)
+    device_id = int(options.device_id)
+    train_name = 'set' + str(validation_set) + '_' + str(image_size)
+
+    print('image size                      :', image_size)
+    print('dataset path                    :', dataset_path)
+    print('batch size (pre-training)       :', batch_size_pre)
+    print('batch size (fine-tuning)        :', batch_size_fine)
+    print('number of epochs (pre-training) :', nb_epoch_pre)
+    print('number of epochs (fine-tuning)  :', nb_epoch_fine)
+    print('output path                     :', output_path)
+    print('gpu device id                   :', device_id)
+    print('train name                      :', train_name)
+
+    # set gpu device
+#    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#    os.environ["CUDA_VISIBLE_DEVICES"] = str(device_id)
+    
+    # The data, shuffled and split between train and test sets:
+    loader = DataLoader(image_size,dataset_path)
+    (X_train, y_train), (X_test, y_test),nb_classes = loader.load_data()
+    print('X_train shape:', X_train.shape)
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+
+    # Convert class vectors to binary class matrices.
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+    X_test = X_test.astype('float32')
+    X_test /= 255
+
+    # create the base pre-trained model
+    base_model = Xception(weights='imagenet', include_top=False)
+
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer and a logistic layer -- let's say we have 25 classes
+    predictions = Dense(nb_classes, activation='softmax')(x)
+
+    # this is the model we will train
+    model = Model(input=base_model.input, output=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional Xception layers
+    for layer in base_model.layers:
         layer.trainable = False
 
-    # TODO: ここでAdamを使うとうまくいかない
-    # Fine-tuningのときは学習率を小さくしたSGDの方がよい？
-    model.compile(loss='binary_crossentropy',
-                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
-                  metrics=['accuracy'])
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    # callbacks
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    logger_pre = CSVLogger(output_path + '/' + train_name + '_training_pre.csv')
+    logger_fine = CSVLogger(output_path + '/' + train_name + '_training_fine.csv')
+    checkpointer = ModelCheckpoint(filepath=output_path + '/' + train_name + '.hdf5', verbose=1, save_best_only=True)
+    
+    ##################################################################################################
+    # pre-training
+    ##################################################################################################
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = createImageDataGenerator()
 
-    test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+    # Compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(X_train)
 
-    train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(f_size,f_size),
-        batch_size=32,
-        class_mode='binary')
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size_pre),
+                        samples_per_epoch=X_train.shape[0],
+                        nb_epoch=nb_epoch_pre,
+                        callbacks=[logger_pre],
+                        validation_data=(X_test, Y_test))
 
-    validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(f_size,f_size),
-        batch_size=32,
-        class_mode='binary')
-    # fitを使う場合
-    history = model.fit(x_train, y_train,
-                    batch_size=10,
-                    epochs=2,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-
-    # Fine-tuning
-    history = model.fit_generator(
-        train_generator,
-        samples_per_epoch=samples_per_epoch,
-        nb_epoch=nb_epoch,
-        validation_data=validation_generator,
-        nb_val_samples=nb_val_samples)
-
-    model.save_weights(os.path.join(result_dir, 'finetuning.h5'))
+#    ##################################################################################################
+#    # fine-tuning
+#    ##################################################################################################
+#    # we chose to train all layers
+#    for layer in model.layers:
+#       layer.trainable = True
+#
+#    # we need to recompile the model for these modifications to take effect
+#    # we use SGD with a low learning rate
+#    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+#
+#    # we train our model again (this time fine-tuning all layers)
+#    print('Using real-time data augmentation.')
+#    # This will do preprocessing and realtime data augmentation:
+#    datagen = createImageDataGenerator()
+#
+#    # Compute quantities required for featurewise normalization
+#    # (std, mean, and principal components if ZCA whitening is applied).
+#    datagen.fit(X_train)
+#
+#    # Fit the model on the batches generated by datagen.flow().
+#    model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size_fine),
+#                        samples_per_epoch=X_train.shape[0],
+#                        nb_epoch=nb_epoch_fine,
+#                        callbacks=[logger_fine, checkpointer],
+#                        validation_data=(X_test, Y_test))
